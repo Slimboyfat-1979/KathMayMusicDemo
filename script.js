d89955a8fc18e1ca50a376f54a5ebdd1ab3b4b90
Slimboyfat-1979 <slimboyfat1979@gmail.com>
2024-07-28 14:03:30 +1200

    Completed main player functionality

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 'use strict'
 
+//Code for music player
+
+const audio = document.querySelector('.audio');
+const playBtn = document.querySelector('.play-btn');
+const backwardBtn = document.querySelector('.backward-btn');
+const forwardBtn = document.querySelector('.forward-btn');
+
+const progressBar = document.querySelector('.progress-bar');
+
+let playingState = false;
+
+playBtn.addEventListener('click', startSong);
+
+function startSong() {
+    if(playingState === false) {
+        playingState = true;
+        playBtn.querySelector('i').className = 'fa-solid fa-pause';
+        audio.play();
+    }else {
+        playingState = false;
+        playBtn.querySelector('i').className = 'fa-solid fa-play';
+        audio.pause();
+    }
+}
+
+audio.addEventListener('timeupdate', function() {
+    const {currentTime, duration} = this;
+    console.log(currentTime, duration);
+    const progressPercent = (currentTime / duration) * 100;
+    progressBar.style.width = `${progressPercent}%`;   
+})
